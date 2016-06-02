7672e09a95e60540583ad12d0692ee8bef9f398e
mars <627556038@qq.com>
2016-06-02 14:04:05 +0800

    Initial Commit

---
+++ MutiSelectTableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MutiSelectTableViewDemo
+//
+//  Created by Guohx on 16/6/2.
+//  Copyright © 2016年 guohx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MutiSelectTableViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MutiSelectTableViewDemo
+//
+//  Created by Guohx on 16/6/2.
+//  Copyright © 2016年 guohx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
